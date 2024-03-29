71ef0a6dca1a84ac6ccdbaf66f3bc3c4fe785910
Yaroslav Salmin <yaroslav.salmin@sky.uk>
2024-03-29 15:22:43 +0000

    typeahead dropdown behaviour with alpinejs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import htmx from 'astro-htmx';
+import alpine from '@astrojs/alpinejs';
 
 import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [htmx()],
+  integrations: [htmx(), alpine()],
   output: 'server',
   adapter: node({
     mode: 'standalone',
